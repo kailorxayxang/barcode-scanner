e6494cfd4334235caac2476bd099fe3d36148c90
skaicit <mylove.lor@gmail.com>
2017-09-15 20:31:05 +0800

    Add All Files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  qrData = null;
+  createdCode = null;
+  scannerCode = null;
+  constructor(public navCtrl: NavController,private barcode:BarcodeScanner) {
+    
+  }
 
-  constructor(public navCtrl: NavController) {
+  createCode() {
+    this.createdCode = this.qrData;
+  }
 
+  scanCode() {
+    this.barcode.scan().then(barcodeData => {
+      this.scannerCode = barcodeData.text;
+    }, (err) => {
+      console.log("ERR", err);
+    });
   }
 
 }
